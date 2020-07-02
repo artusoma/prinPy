90ff99be2922debd6d83fc236a02aba06cee870c
artusoma <artusoma@outlook.com>
2020-07-02 11:00:10 -0400

    added setup, gitignore for package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name = 'prinpy',
+    version = '0.0.2.2',
+    license = "MIT",
+    description = "A package for fitting principal curves in Python",
+    author = "https://github.com/artusoma/",
+    author_email = 'artusoma1@gmail.com',
+    url = 'https://github.com/artusoma/prinPy', 
+    packages = ["prinpy"],
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    install_requires = ['numpy',
+                        'matplotlib',
+                        'scipy',
+                        ],
+)
